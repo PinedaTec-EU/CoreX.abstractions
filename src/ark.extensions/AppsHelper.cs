1dd93af2652f41b47d0a094405a667a821bb7a2f
jmrpineda <jmr.pineda@gmail.com>
2025-02-07 01:42:46 +0100

    Adding headers file

--- src/ark.extensions/AppsHelper.cs
+++ src/ark.extensions/AppsHelper.cs
@@ -1,4 +1,32 @@
-﻿using System.Collections;
+#region Header
+
+// --------------------------------------------------------------------------------------
+// Powered by:
+// 
+//     __________.__                  .___    ___________                             
+//     \______   \__| ____   ____   __| _/____\__    ___/___   ____       ____  __ __ 
+//      |     ___/  |/    \_/ __ \ / __ |\__  \ |    |_/ __ \_/ ___\    _/ __ \|  |  \
+//      |    |   |  |   |  \  ___// /_/ | / __ \|    |\  ___/\  \___    \  ___/|  |  /
+//      |____|   |__|___|  /\___  >____ |(____  /____| \___  >\___  > /\ \___  >____/ 
+//                   \/     \/     \/     \/           \/     \/  \/     \/
+// 
+// 
+// FileName: AppsHelper.cs
+//
+// Author:   jmr.pineda
+// eMail:    jmr.pineda@pinedatec.eu
+// Profile:  http://pinedatec.eu/profile
+//
+//           Copyrights (c) PinedaTec.eu 2025, all rights reserved.
+//           CC BY-NC-ND - https://creativecommons.org/licenses/by-nc-nd/4.0
+//
+//  Created at: 2025-02-07T00:41:48.094Z
+//
+// --------------------------------------------------------------------------------------
+
+#endregion
+
+using System.Collections;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
--- test/ark.abstractions.test/AppsHelperTest.cs
+++ test/ark.abstractions.test/AppsHelperTest.cs
@@ -1,4 +1,32 @@
-﻿using System.Text;
+#region Header
+
+// --------------------------------------------------------------------------------------
+// Powered by:
+// 
+//     __________.__                  .___    ___________                             
+//     \______   \__| ____   ____   __| _/____\__    ___/___   ____       ____  __ __ 
+//      |     ___/  |/    \_/ __ \ / __ |\__  \ |    |_/ __ \_/ ___\    _/ __ \|  |  \
+//      |    |   |  |   |  \  ___// /_/ | / __ \|    |\  ___/\  \___    \  ___/|  |  /
+//      |____|   |__|___|  /\___  >____ |(____  /____| \___  >\___  > /\ \___  >____/ 
+//                   \/     \/     \/     \/           \/     \/  \/     \/
+// 
+// 
+// FileName: AppsHelperTest.cs
+//
+// Author:   jmr.pineda
+// eMail:    jmr.pineda@pinedatec.eu
+// Profile:  http://pinedatec.eu/profile
+//
+//           Copyrights (c) PinedaTec.eu 2025, all rights reserved.
+//           CC BY-NC-ND - https://creativecommons.org/licenses/by-nc-nd/4.0
+//
+//  Created at: 2025-02-07T00:41:48.095Z
+//
+// --------------------------------------------------------------------------------------
+
+#endregion
+
+using System.Text;
 using ark.abstractions.test;
 using ark.extensions;
 
